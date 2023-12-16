cb9a02cddf1e031122619121eb41e389d76583e8
mohammed236 <melladmhammed200@gmail.com>
2023-12-16 17:14:25 +0100

    Trying to Sign In to The Git Repo

--- L7oma/Assets/Scripts/PrefabsMoving.cs
+++ L7oma/Assets/Scripts/PrefabsMoving.cs
@@ -3,21 +3,9 @@ using Unity.Netcode;
 using UnityEngine;
 public class PrefabsMoving : NetworkBehaviour
 {
-    public override void OnNetworkSpawn()
+    /*public override void OnNetworkSpawn()
     {
-        ShootingSystem.Instance.OnObjectSpawned += Move;
-    }
-    public void Move(Transform cameraPosition,List<Weapon_SO> weapon_SOs,int weaponIndex,float rangeModefier,float holdTime)
-    {
-        Vector3 moveDirection = (cameraPosition.up * weapon_SOs[weaponIndex].upModefier + cameraPosition.forward * weapon_SOs[weaponIndex].forwadModefier)
-            * weapon_SOs[weaponIndex].range * rangeModefier * holdTime;
-
-        MoveOnServerRpc(moveDirection);
-    }
-    [ServerRpc]
-    private void MoveOnServerRpc(Vector3 moveDirection)
-    {
-        GetComponent<Rigidbody>().AddForce(moveDirection);
-        ShootingSystem.Instance.OnObjectSpawned -= Move;
-    }
+        base.OnNetworkSpawn();
+        transform.Translate(Vector3.back * 5f);
+    }*/
 }
--- L7oma/Assets/Scripts/ShootingSystem.cs
+++ L7oma/Assets/Scripts/ShootingSystem.cs
@@ -4,8 +4,6 @@ using System;
 using Unity.Netcode;
 public class ShootingSystem : NetworkBehaviour
 {
-    public static ShootingSystem Instance { get; private set; }
-
     [SerializeField] private Transform startPointTransform;
     [SerializeField] private List<Weapon_SO> weapon_SOs = new List<Weapon_SO>();
     [SerializeField] private float rangeModefier = 10;
@@ -13,40 +11,7 @@ public class ShootingSystem : NetworkBehaviour
     private int weaponIndex = 0;
     [Range(0f,1f)]
     private float holdTime = 0;
-
-    public event Action<Transform, List<Weapon_SO> , int , float , float > OnObjectSpawned;
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-            Instance = this;
-    }
-
-    //shoot projectiles
-    private void Shoot()
-    {
-        //spawn it on the network on the host and move it
-        SpawnPrefabOnServerRpc(startPointTransform.position, Quaternion.identity);
-    }   
-
-    [ServerRpc]
-    private void SpawnPrefabOnServerRpc(Vector3 pos,Quaternion rot)
-    {
-        GameObject InstantiatedObject = Instantiate(weapon_SOs[weaponIndex].projectilePrefab, pos, rot);
-
-        NetworkObject networkObject = InstantiatedObject.GetComponent<NetworkObject>();
-        networkObject.Spawn();
-
-        Transform cameraPosition = GetComponentInChildren<Camera>().transform;
-
-        OnObjectSpawned?.Invoke(cameraPosition, weapon_SOs, weaponIndex, rangeModefier, holdTime);
-
-        Destroy(InstantiatedObject,3f);
-    }
+    
     private void Update()
     {
         if (!IsOwner) return;
@@ -59,9 +24,28 @@ public class ShootingSystem : NetworkBehaviour
         }else if (InputsData.IsShootingReleased())
         {
             //shoot
-            Shoot();
+            SpawnAndMovePrefabOnServerRpc(startPointTransform.position, Quaternion.identity);
 
             holdTime = 0;
         }
+    }
+    [ServerRpc]
+    private void SpawnAndMovePrefabOnServerRpc(Vector3 pos, Quaternion rot)
+    {
+        GameObject InstantiatedObject = Instantiate(weapon_SOs[weaponIndex].projectilePrefab, pos, rot);
+        NetworkObject networkObject = InstantiatedObject.GetComponent<NetworkObject>();
+        networkObject?.Spawn();
+
+        Move(networkObject.NetworkObjectId);
+        Destroy(InstantiatedObject, 3f);
+    }
+    void Move(ulong id)
+    {
+
+        Transform cameraPosition = GetComponentInChildren<Camera>().transform;
+        Vector3 moveDirection = (cameraPosition.up * weapon_SOs[weaponIndex].upModefier + cameraPosition.forward * weapon_SOs[weaponIndex].forwadModefier)
+            * weapon_SOs[weaponIndex].range * rangeModefier * holdTime;
+
+        GetNetworkObject(id)?.GetComponent<Rigidbody>().AddForce(moveDirection);
     }
 }
